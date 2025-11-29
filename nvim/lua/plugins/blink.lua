5573e1eabd284d1f11d3851a60f34f88cf6bcd04
Jordan Texier <jordan31891@gmail.com>
2025-11-29 10:32:48 -0500

    add new nvim plugins

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  'saghen/blink.cmp',
+  opts = {
+    keymap = { preset = 'default'},
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    signature = {
+      enabled = false,
+    },
+    fuzzy = { implementation = "lua" }
+  },
+}
